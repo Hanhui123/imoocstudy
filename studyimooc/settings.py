6aeb5b698ed60c7edef1377814fc6cb85599a906
qianqian77 <guoqianqian@seclover.com>
2018-11-07 17:50:33 +0800

    设计了所有的model。

--- studyimooc/settings.py
+++ studyimooc/settings.py
@@ -16,9 +16,9 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#设置apps,extra_apps 目录
-sys.path.insert(0,os.path.join(BASE_DIR, 'apps'))
-sys.path.insert(0,os.path.join(BASE_DIR, 'extra_apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+# sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -40,11 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.users',
-    'apps.courses',
-    'apps.operation',
-    'apps.organization',
-
+    'users.apps.UsersConfig',
+    'courses.apps.CoursesConfig',
+    'operation.apps.OperationConfig',
+    'organization.apps.OrganizationConfig',
 
 ]
 
@@ -57,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'studyimooc.urls'
 
 TEMPLATES = [
@@ -84,9 +82,13 @@ WSGI_APPLICATION = 'studyimooc.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'imooc',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
@@ -113,15 +115,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -129,7 +133,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_FILES = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
+#告诉Django认证这个表
+AUTH_USER_MODEL = 'users.UserProfile'
